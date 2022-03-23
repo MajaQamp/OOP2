c1de5cabbf530a22697f99d2eefb9ef1202b46cd
MajaQamp <98764757+MajaQamp@users.noreply.github.com>
2022-03-23 14:05:08 +0100

    Update CoffeeMachine.java

--- OOP2/CoffeeMachine.java
+++ OOP2/CoffeeMachine.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     private ArrayList<String> coffeeBrands;
-    Scanner reader=new Scanner(System.in);
+    Scanner reader = new Scanner(System.in);
 
     public CoffeeMachine(ArrayList<String> coffeeBrands) {
         this.coffeeBrands = coffeeBrands;
@@ -19,20 +19,16 @@ public class CoffeeMachine {
                 "\n3 for Segafredo" +
                 "\n4 for Manuel");
         Integer userInput = Integer.valueOf(reader.next());
-            if (userInput==1){
-                System.out.println("You have selected Lavazza");
-            }
-            else if (userInput==2){
-                System.out.println("You have selected Illy");
-            }
-            else if (userInput==3){
-                System.out.println("You have selected Segafredo");
-            }
-            else if (userInput==4){
-                System.out.println("You have selected Manuel");
-            }
-
+        if (userInput == 1) {
+            System.out.println("You have selected Lavazza");
+        } else if (userInput == 2) {
+            System.out.println("You have selected Illy");
+        } else if (userInput == 3) {
+            System.out.println("You have selected Segafredo");
+        } else if (userInput == 4) {
+            System.out.println("You have selected Manuel");
         }
 
     }
 
+}
